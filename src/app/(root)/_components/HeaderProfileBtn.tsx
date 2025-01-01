22f90e8ca277168c639ed42f1301ced8a63eeb7d
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-01 09:57:02 +0530

    Header Profile Btn. comp. for Header

---
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -0,0 +1,26 @@
+"use client";
+import LoginButton from "@/components/LoginButton";
+import { SignedOut, UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
+function HeaderProfileBtn() {
+  return (
+    <>
+    {/* we coould have used only `<UserButton />` but since we wanted to add a custom link, we used  */}
+      <UserButton> 
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="Profile"
+            labelIcon={<User className="size-4" />}
+            href="/profile"
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+
+      <SignedOut>
+        <LoginButton />
+      </SignedOut>
+    </>
+  );
+}
+export default HeaderProfileBtn;
