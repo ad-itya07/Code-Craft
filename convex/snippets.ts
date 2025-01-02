cc84df05361b95b1e3e4165997051a04903e330b
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-02 15:07:04 +0530

    added: Get-Snippet, is-Snippet-Starred, get-Snippet-Star-Count query

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const createSnippet = mutation({
   args: {
@@ -28,5 +28,48 @@ export const createSnippet = mutation({
     });
 
     return snippetId;
+  },
+});
+
+export const getSnippets = query({
+  // By default, the order desc/asc depends on the creation-time
+  handler: async (ctx) => {
+    const snippets = await ctx.db.query("snippets").order("desc").collect();
+    return snippets;
+  },
+});
+
+export const isSnippetStarred = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) return false;
+
+    const star = await ctx.db
+      .query("stars")
+      .withIndex("by_user_id_and_snippet_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identity.subject) &&
+          q.eq(q.field("snippetId"), args.snippetId)
+      )
+      .first();
+
+    return !!star; // (star) itself is an object, but we have to get a boolen, so we use `!!` to convert it to a boolean;
+  },
+});
+
+export const getSnippetStarCount = query({
+  args: { snippetId: v.id("snippets") },
+  handler: async (ctx, args) => {
+    const stars = await ctx.db
+      .query("stars")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .collect();
+
+    return stars.length;
   },
 });
