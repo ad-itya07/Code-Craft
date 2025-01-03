1ff18a1a60bfffcec2eb94ac00cb8457158ad307
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-03 15:44:39 +0530

    added: get-Snippet-By-Id, get-Comments query

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -39,6 +39,30 @@ export const getSnippets = query({
   },
 });
 
+export const getSnippetById = query({
+  args: { snippetId: v.id("snippets") },
+  handler: async (ctx, args) => {
+    const snippet = await ctx.db.get(args.snippetId);
+    if (!snippet) throw new Error("Snippet not found");
+
+    return snippet;
+  },
+});
+
+export const getComments = query({
+  args: { snippetId: v.id("snippets") },
+  handler: async (ctx, args) => {
+    const comments = await ctx.db
+      .query("snippetComments")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .order("desc")
+      .collect();
+
+    return comments;
+  },
+});
+
 export const isSnippetStarred = query({
   args: {
     snippetId: v.id("snippets"),
@@ -127,7 +151,8 @@ export const starSnippet = mutation({
       .withIndex("by_user_id_and_snippet_id")
       .filter(
         (q) =>
-          q.eq(q.field("userId"), identity.subject) && q.eq(q.field("snippetId"), args.snippetId)
+          q.eq(q.field("userId"), identity.subject) &&
+          q.eq(q.field("snippetId"), args.snippetId)
       )
       .first();
 
