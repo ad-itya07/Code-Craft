d9e52d8c96cb74005362959925c8dd580d5b88aa
ad_itya07 <jhaaditya2207@gmail.com>
2025-01-06 13:12:30 +0530

    integrated checkout url

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -2,10 +2,11 @@ import { Zap } from "lucide-react";
 import Link from "next/link";
 
 export default function UpgradeButton() {
-  // TODO: Implement payments using lemonSqueezy
+  const CHECKOUT_URL =
+    "https://store-craft.lemonsqueezy.com/buy/a0d2f3a3-7ae1-422b-bb8d-761262edbd21";
   return (
     <Link
-      href={"/"}
+      href={CHECKOUT_URL}
       className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
         bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
         hover:from-blue-600 hover:to-blue-700 transition-all"
